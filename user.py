b009578ed1b7bdf9bddc95fca3e6789b3dcdab94
brandontaylor156 <brandon.david4@outlook.com>
2022-07-03 22:37:48 -0700

    modified for chain methods

--- user.py
+++ user.py
@@ -10,22 +10,26 @@ class User:
     def display_info(self):
         print(f"First Name: {self.first_name}\nLast Name: {self.last_name}\nEmail: {self.email}\nAge: {self.age}\nRewards Member: {self.is_rewards_member}\nGold Card Points: {self.gold_card_points}")
         print("\n")
+        return self
 
     def enroll(self):
         if self.is_rewards_member is True:
             print("Already a member")
-            return
-        self.is_rewards_member = True
-        self.gold_card_points = 200
+        else:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+        return self
+
 
     def spend_points(self, amount):
         if self.gold_card_points - amount < 0:
             print("Not enough points")
-            return
-        self.gold_card_points -= amount
+            
+        else:
+            self.gold_card_points -= amount
+        return self
 
 myUser = User("Brandon", "Taylor", "brandon.david4@outlook.com", 26) 
-myUser.enroll()
 # myUser.display_info()
 
 newUser1 = User("Kobe", "Bryant", "kobe@gmail.com", 24)
@@ -34,15 +38,6 @@ newUser1 = User("Kobe", "Bryant", "kobe@gmail.com", 24)
 newUser2 = User("Lebron", "James", "lebron@gmail.com", 23)
 # newUser2.display_info()
 
-myUser.spend_points(50)
-# myUser.display_info()
-
-newUser1.enroll()
-newUser1.spend_points(80)
-
-myUser.display_info()
-newUser1.display_info()
-newUser2.display_info()
-
-myUser.enroll()
-newUser2.spend_points(40)
+myUser.enroll().spend_points(50).display_info()
+newUser1.enroll().spend_points(80).display_info()
+newUser2.display_info().spend_points(40).enroll().spend_points(40).display_info()
